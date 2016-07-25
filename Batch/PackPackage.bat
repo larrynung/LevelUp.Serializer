cf3c1c90efb2c3d7c035ce6e94dad8c384c0d98b
larry nung <larrynung@hotmail.com>
2016-07-25 18:11:44 +0800

    Upgrade NuGet.exe and adjust nuget package setting

--- Batch/PackPackage.bat
+++ Batch/PackPackage.bat
@@ -1,3 +1,3 @@
 @echo off
-nuget pack -build -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=../../Bin/Release" -OutputDirectory "../../Package"
+nuget pack -build -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=../../Bin/Release" -OutputDirectory "../../Package" 
 pause
